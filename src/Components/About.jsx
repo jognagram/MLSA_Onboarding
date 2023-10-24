fbc78414437bf95e85b186b99675e7b1e1dcfbdf
jogna <jordan.gnamien@smile.ci>
2023-10-24 20:24:30 +0000

    Changement

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "wood background beautiful and simple";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a student in MCs in Computer Science at AGITEL-FORMATION Côte d'Ivoire. I enjoy creating unique and simplistic user interfaces in creative ways."; 
 
 /**
  * List of some of skills or technologies you work on, are learning,
